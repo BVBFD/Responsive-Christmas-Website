10918512ca8388b2a77507b294f258bb0fd136c1
BVBFD <lsevina126@gmail.com>
2021-12-28 19:05:58 +0900

    Code About part

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ window.onscroll = () => {
   document.querySelector(".home .cloud-1").style.right = `${-value}%`;
   document.querySelector(".home .cloud-2").style.left = `${-value}%`;
 };
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 650) {
+//     document.querySelector(".header").classList.add("active");
+//   } else {
+//     document.querySelector(".header").classList.remove("active");
+//   }
+
+//   let value = window.scrollY / 10;
+//   document.querySelector(".home .mountain").style.bottom = `${-value}%`;
+//   document.querySelector(".home .content").style.bottom = `${value}%`;
+//   document.querySelector(".home .moon").style.marginTop = `${-value}%`;
+//   document.querySelector(".home .cloud-1").style.right = `${-value}%`;
+//   document.querySelector(".home .cloud-2").style.left = `${-value}%`;
+// });
