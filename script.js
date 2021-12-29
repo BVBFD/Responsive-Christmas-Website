c6c5c6e10180cbf991ce98adad538ed16e65fc09
BVBFD <lsevina126@gmail.com>
2021-12-30 06:53:27 +0900

    Code Footer Part and Mobile Screen Optimization

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ let searchForm = document.querySelector(".header .search-form");
 let menuBtn = document.querySelector("#menu-btn");
 let navbar = document.querySelector("header .navbar");
 
+searchBtn.onclick = () => {
+  searchBtn.classList.toggle("fa-times");
+  searchForm.classList.toggle("active");
+  menuBtn.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+  searchBtn.classList.remove("fa-times");
+  searchForm.classList.remove("active");
+};
+
 window.onscroll = () => {
   if (window.scrollY > 650) {
     document.querySelector(".header").classList.add("active");
